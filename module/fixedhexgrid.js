c7f60561d40f28342948e6613162eba80dff7086
Joe Nosie <182554+BoltsJ@users.noreply.github.com>
2021-09-09 19:08:40 -0500

    Update for known problems on 0.8.x Hex grids

--- module/fixedhexgrid.js
+++ module/fixedhexgrid.js
@@ -1,32 +1,35 @@
 export class FixedHexagonalGrid extends HexagonalGrid {
-  constructor(...args) {
-    super(...args);
-    this.w = (this.w * 2) / Math.sqrt(3);
-    this.h = (this.h * 2) / Math.sqrt(3);
+  constructor(options) {
+    super(options);
+    const size = options.dimensions.size;
+    if (this.columns) {
+      this.w = (size * 2) / Math.sqrt(3);
+      this.h = size;
+    } else {
+      this.w = size;
+      this.h = (size * 2) / Math.sqrt(3);
+    }
   }
 
-  getSnappedPosition(x, y, interval = 1) {
-    // Only use new logic for interval 2, which is used for tmplates and map pins
-    if (interval !== 2) return super.getSnappedPosition(x, y, interval);
+  /**
+   * See https://www.redblobgames.com/grids/hexagons/ for reference
+   * @override
+   */
+  cubeToOffset(q, r, s) {
+    const offset = this.options.even ? -1 : 1;
 
-    const [x0, y0] = this.getCenter(x, y);
+    // Column orientation
+    if (this.options.columns) {
+      const col = q;
+      const row = s + (q - offset * (q & 1)) / 2;
+      return { row, col };
+    }
 
-    // Find the nearest point in the set of the center and vertices
-    // get an array of points representing the vertices of the cell
-    const points = this.hexPoints.map(p => {
-      return { x: x0 + (p[0] - 0.5) * this.w, y: y0 + (p[1] - 0.5) * this.h };
-    });
-
-    // Find the closest point to the snap location
-    return points.reduce(
-      (r, p) => {
-        if ((x - p.x) ** 2 + (y - p.y) ** 2 >= (x - r.x) ** 2 + (y - r.y) ** 2) {
-          return r;
-        } else {
-          return p;
-        }
-      },
-      { x: x0, y: y0 }
-    );
+    // Row orientation
+    else {
+      const row = s;
+      const col = q + (s - offset * (s & 1)) / 2;
+      return { row, col };
+    }
   }
 }
